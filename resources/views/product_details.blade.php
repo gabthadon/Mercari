56792269a05e899534f6f9baee732ffa797e6e86
gabthadon <dzangalist@gmail.com>
2022-06-16 12:24:34 +0100

    Added star rating

--- resources/views/product_details.blade.php
+++ resources/views/product_details.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script src="https://unpkg.com/vue-star-rating/dist/VueStarRating.umd.min.js"></script>
+
     <link rel="stylesheet" href="/slick/slick.css">
     <link rel="stylesheet" href="/slick/slick-theme.css">
       <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -9,6 +11,7 @@
       <link rel="shortcut icon" href="/images/logos/gadgeduplogo-2.jpg" type="image/x-icon">
       <link rel="stylesheet" href="/css/app.css">
       <link rel="stylesheet" href="/css/style.css">
+      <link rel="stylesheet" href="/css/all.min.css">
      
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -48,6 +51,10 @@
     <div class="prod_title">Meet the seller</div>
 <br>
     <img class="seller_image" src="/storage/{{$data->user->avatar}}" alt="" srcset=""> <span class="prod_title">{{$data->user->name}}</span>
+   <div id='star-rating'>
+   <star-rating v-model="rating"></star-rating>
+   </div>
+   <row-rating v-model='rating' :increment="0.5" text-class="custom-text"></row-rating>
 </div>
 </div>
 
@@ -88,5 +95,6 @@
 
 
 <script src="/js/app.js"></script>
+
 </body>
 </html>
