0fdbd790bff6746e9ca6a3cb9d78a55b23649954
gabthadon <dzangalist@gmail.com>
2022-06-17 15:10:42 +0100

    make sure users agree to terms of service before creating account

--- public/js/terms.js
+++ public/js/terms.js
@@ -2,5 +2,9 @@
 var btn_terms = document.getElementById("termsandcon");
 var agree= document.getElementById("agree");
 agree.addEventListener("change", function(){
+    if(btn_terms.disabled==true){
 btn_terms.disabled=false;
+}else{
+    btn_terms.disabled=true;
+}
  })
